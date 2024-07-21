a52f29242e5f34d7b520a68fa9ad4858522895f4
aluhn <aluhn2012@gmail.com>
2024-07-21 11:41:20 -0400

    Update route1.asm

--- data/wildPokemon/route1.asm
+++ data/wildPokemon/route1.asm
@@ -1,12 +1,12 @@
 Route1Mons:
 	db $19
-	db 3,PIDGEY
-	db 3,RATTATA
-	db 3,RATTATA
-	db 2,RATTATA
-	db 2,PIDGEY
-	db 3,PIDGEY
-	db 3,PIDGEY
+	db 12,PIDGEY
+	db 12,RATTATA
+	db 12,RATTATA
+	db 12,RATTATA
+	db 12,PIDGEY
+	db 12,PIDGEY
+	db 12,PIDGEY
 	db 4,RATTATA
 	db 4,PIDGEY
 	db 5,PIDGEY
